70eb634953596ab7abbae42c2fb1ac9f7ef38f71
she1nXgod <domenator156@mail.ru>
2026-04-04 00:07:09 +0300

    add count messages

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,22 +1,25 @@
 import Stack from 'react-bootstrap/Stack';
-import { getChannels } from '../api/chatApi';
+import { getChannels, getMessages } from '../api/chatApi';
 import { useSelector } from 'react-redux';
 import { selectCurrentChannel } from '../slices/uiSlice';
 import LoadingSpinner from './LoadingSpinner';
 
 const ChatHeader = () => {
-  const { data: channels, isLoading } = getChannels();
+  const { data: channels, isLoading: isLoadingChannels } = getChannels();
+  const { data: messages, isLoading: isLoadingMessages } = getMessages();
   const currentChannelId = useSelector(selectCurrentChannel);
-  const currentChannelName = channels?.find(({ id }) => id === currentChannelId);
 
-  if (isLoading) {
+  const channelName = channels?.find(({ id }) => id === currentChannelId);
+  const channelCountMessages = messages?.filter(({ channelId }) => channelId === currentChannelId).length;
+
+  if (isLoadingChannels || isLoadingMessages) {
     return <LoadingSpinner />;
   }
 
   return (
     <Stack gap={0}>
-      <b>{`# ${currentChannelName.name}`}</b>
-      <span className="small text-muted">сообщений: ???</span>
+      <b>{`# ${channelName.name}`}</b>
+      <span className="small text-muted">{`сообщений: ${channelCountMessages}`}</span>
     </Stack>
   );
 };
