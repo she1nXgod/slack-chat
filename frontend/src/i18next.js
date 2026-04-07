6d91a06a6e45234bf52e15ec0958bdf35b3bd085
she1nXgod <domenator156@mail.ru>
2026-04-08 02:15:48 +0300

    fix

--- frontend/src/i18next.js
+++ frontend/src/i18next.js
@@ -2,15 +2,13 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import resources from './locales/index.js';
 
-const initI18n = () => {
-  return i18n.use(initReactI18next).init({
-    resources,
-    lng: 'ru',
-    fallbackLng: 'ru',
-    interpolation: {
-      escapeValue: false,
-    },
-  });
-};
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'ru',
+  fallbackLng: 'ru',
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
-export default initI18n;
+export default i18n;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
-import initI18n from './i18next.js';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18next.js';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
@@ -9,23 +10,18 @@ import { rollbarConfig } from './rollbar.js';
 import App from './App.jsx';
 import store from './store.js';
 import { ToastContainer } from 'react-toastify';
-import { I18nextProvider } from 'react-i18next';
 
-const root = createRoot(document.getElementById('root'));
-
-initI18n().then((i18nInstance) => {
-  root.render(
-    <StrictMode>
-      <RollbarProvider config={rollbarConfig}>
-        <ErrorBoundary>
-          <ReduxProvider store={store}>
-            <I18nextProvider i18n={i18nInstance}>
-              <App />
-              <ToastContainer autoClose={2500} />
-            </I18nextProvider>
-          </ReduxProvider>
-        </ErrorBoundary>
-      </RollbarProvider>
-    </StrictMode>,
-  );
-});
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <ReduxProvider store={store}>
+          <I18nextProvider i18n={i18n}>
+            <App />
+            <ToastContainer autoClose={2500} />
+          </I18nextProvider>
+        </ReduxProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
+  </StrictMode>,
+);
